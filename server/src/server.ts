11acc18db761727b08e0b5a94c2b3068bb253bdc
Pratik Nikam <nikampratik2989@gmail.com>
2025-07-19 23:50:24 +0530

    hi

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,7 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(cors());
+
 app.use(express.static(path.join(__dirname, "public"))) // Serve static files
 
 const server = http.createServer(app)
