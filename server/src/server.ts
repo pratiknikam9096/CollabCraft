33d6249b323cfbb1d3796865fe51c7c0e679631b
Pratik Nikam <nikampratik2989@gmail.com>
2025-08-08 17:50:27 +0530

    Now slove Issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ app.use(express.json())
 
 app.use(cors({
  origin: [
-	'https://collabcraft-cbqu.onrender.com','http://localhost:5173',
+	'https://collabcraft-cbqu.onrender.com',
   ], // Replace with your deployed frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
@@ -32,12 +32,9 @@ const server = http.createServer(app)
 const io = new Server(server, {
 	cors: {
 		 origin: [
-<<<<<<< HEAD
-			'localhost:5173',
+
 		'https://collabcraft-cbqu.onrender.com'
-=======
-      'https://collabcraft-cbqu.onrender.com', 'http://localhost:5173',
->>>>>>> 2059fca8006f49612ea35e12e7c4b5e1b51251e6
+
     ], // Replace with your deployed frontend URL
         methods: ['GET', 'POST','PUT', 'DELETE', 'PATCH'],
 	},
