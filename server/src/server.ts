b364dd0a588319633063e7bed42d22499df0f94d
Arjun Veer <136682903+arjun-veer@users.noreply.github.com>
2025-08-08 10:23:28 +0530

    Updated server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ app.use(express.json())
 
 app.use(cors({
  origin: [
-	'https://collabcraft-cbqu.onrender.com'
+	'https://collabcraft-cbqu.onrender.com', 'https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--96435430.local-credentialless.webcontainer-api.io/'
   ], // Replace with your deployed frontend URL
   methods: ['GET', 'POST'],
   credentials: true
