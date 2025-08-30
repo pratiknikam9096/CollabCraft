4e5542538e3c017f049d09e8d6cec1e15d013fb6
Pratik Nikam <nikampratik2989@gmail.com>
2025-08-30 11:33:27 +0530

    Now work Properly

--- client/src/components/GitHubCorner.tsx
+++ client/src/components/GitHubCorner.tsx
@@ -1,19 +1,20 @@
-// import { VIEWS } from "@/types/view"
-// import SidebarButton from "@/components/sidebar/sidebar-views/SidebarButton"
-// import { useViews } from "@/context/ViewContext"
+import { VIEWS } from "@/types/view"
+import { useViews } from "@/context/ViewContext"
 import { useRunCode } from "@/context/RunCodeContext"
 
-
 function GitHubCorner() {
-//   const { viewIcons } = useViews()
-  const runCtx = useRunCode() // returns RunContext object
+  const { viewIcons } = useViews()
+  const runCtx = useRunCode()
 
   return (
     <div className="fixed right-0 top-0 z-10 flex items-start gap-2">
-      {/* GitHub Corner */}
-      {/* //<SidebarButton viewName={VIEWS.RUN} icon={viewIcons[VIEWS.RUN]}  /> */}
-      <button onClick={runCtx.runCode}>Run</button>
-      {/* or: <button onClick={() => runCtx.run()}>Run</button> */}
+      {/* Just icon, clickable */}
+      <button
+        onClick={runCtx.runCode}
+        className="p-2 rounded-full hover:bg-gray-200 transition"
+      >
+        {viewIcons[VIEWS.RUN]}
+      </button>
     </div>
   )
 }
