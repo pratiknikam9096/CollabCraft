11f31439ec5f3917b74eef84d5d0ac2100d61c80
Pratik Nikam <nikampratik2989@gmail.com>
2025-08-30 11:46:44 +0530

    video call working

--- client/src/components/GitHubCorner.tsx
+++ client/src/components/GitHubCorner.tsx
@@ -3,14 +3,18 @@ import { useViews } from "@/context/ViewContext"
 import { useRunCode } from "@/context/RunCodeContext"
 
 function GitHubCorner() {
-  const { viewIcons } = useViews()
+  const { viewIcons, setCurrentView } = useViews() // get setter for sidebar view
   const runCtx = useRunCode()
+
+  const handleRunClick = () => {
+    setCurrentView(VIEWS.RUN)  // open sidebar to RUN tab
+    runCtx.runCode()           // then run code
+  }
 
   return (
     <div className="fixed right-0 top-0 z-10 flex items-start gap-2">
-      {/* Just icon, clickable */}
       <button
-        onClick={runCtx.runCode}
+        onClick={handleRunClick}
         className="p-2 rounded-full hover:bg-gray-200 transition"
       >
         {viewIcons[VIEWS.RUN]}
