44124a143c6f15d1dddfc64cb1ffd4902e6d0cae
Pratik Nikam <nikampratik2989@gmail.com>
2025-09-07 15:43:57 +0530

    ok

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,22 @@
-import { Route, BrowserRouter as Router, Routes, useLocation } from "react-router-dom";
-import Toast from "./components/toast/Toast";
-import EditorPage from "./pages/EditorPage";
-import HomePage from "./pages/HomePage";
-import NotFound from "./pages/NotFound";
-import VideoCallOverlay from "./components/video/VideoCallOverlay";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+// import GitHubCorner from "./components/GitHubCorner"
+import Toast from "./components/toast/Toast"
+import EditorPage from "./pages/EditorPage"
+import HomePage from "./pages/HomePage"
+import NotFound from "./pages/NotFound"
 
-const AppContent = () => {
-    const location = useLocation();
-    // Show overlay only on /editor/:roomId
-    const isEditorRoom = location.pathname.startsWith("/editor/");
-
+const App = () => {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/editor/:roomId" element={<EditorPage />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-            {isEditorRoom && <VideoCallOverlay onClose={() => {}} />}
-            <Toast />
+            <Router basename="/">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/editor/:roomId" element={<EditorPage />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Router>
+            <Toast /> {/* Toast component from react-hot-toast */}
         </>
-    );
-};
-
-const App = () => (
-    <Router basename="/">
-        <AppContent />
-    </Router>
-);
-
-export default App;
+    )
+}
+export default App
